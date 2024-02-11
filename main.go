d9ccc581bc11b49cdb2a5da77b7d97736eeb16fe
trinitty999 <hanansmani@gmail.com>
2024-02-12 00:21:45 +0100

    trying with go.

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	//"github.com/diskfs/go-diskfs"	
+)
+
+func main(){
+	fmt.Println("Hello world!")
+}
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <filesystem>
-using namespace std;
-
-int main (int argc, char ** argv){
-    cout << "Hello world\n" << endl;
-    return 0;
-}
