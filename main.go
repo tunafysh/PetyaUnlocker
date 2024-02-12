24e14af0ae15f51e0df667d818aefb404fc4bab0
Trinitty999 <hanansmani@gmail.com>
2024-02-12 16:08:33 +0100

    commit message

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"os/user"
+	"regexp"
 )
 
 func ClearScr() {
@@ -15,15 +16,16 @@ func ClearScr() {
 }
 
 func isRoot() bool {
-    currentUser, err := user.Current()
-    if err != nil {
-        fmt.Println("\033[1;31m==> \033[97mUnable to get user.")
-    }
-    return currentUser.Username == "root"
+	currentUser, err := user.Current()
+	if err != nil {
+		fmt.Println("\033[1;31m==> \033[97mUnable to get user mode. Aborting.")
+		os.Exit(2)
+	}
+	return currentUser.Username == "root"
 }
 
 func main() {
-	ClearScr()	
+	ClearScr()
 	var isrootuser bool = isRoot()
 
 	if !isrootuser {
@@ -32,5 +34,30 @@ func main() {
 	}
 
 	fmt.Println("\033[1;32m==> \033[97mHello world!")
-	
+
+	lscmd := exec.Command("ls", "/dev")
+	lsout, lserr := lscmd.CombinedOutput()
+
+	if lserr != nil {
+		fmt.Println("\033[1;31m==> \033[97mCommand threw error. Cannot continue.")
+		os.Exit(3)
+	}
+
+	lsoutput := string(lsout)
+	lscmd.Run()
+
+	pattern := `sd[a-z][0-9]`
+
+	re := regexp.MustCompile(pattern)
+
+	drives := re.FindStringSubmatch(lsoutput)
+
+	for _, drive := range drives {
+		// Use the regular expression to check for a match
+		if re.MatchString(drive) {
+			fmt.Printf("%s\n", drive)
+		} else {
+			fmt.Printf("%s\n", drive)
+		}
+	}
 }
