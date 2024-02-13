2ee21b05c2bf9869d44608a935fc0ad2c21b4cce
Trinitty999 <hanansmani@gmail.com>
2024-02-13 11:28:26 +0100

    added shell script

--- main/main.go
+++ main/main.go
@@ -5,7 +5,6 @@ import (
 	// "github.com/diskfs/go-diskfs"
 	"os"
 	"os/exec"
-	"os/user"
 	"regexp"
 )
 
@@ -15,23 +14,8 @@ func ClearScr() {
 	c.Run()
 }
 
-func isRoot() bool {
-	currentUser, err := user.Current()
-	if err != nil {
-		fmt.Println("\033[1;31m==> \033[97mUnable to get user mode. Aborting.")
-		os.Exit(2)
-	}
-	return currentUser.Username == "root"
-}
-
 func main() {
 	ClearScr()
-	var isrootuser bool = isRoot()
-
-	if !isrootuser {
-		fmt.Println("\033[1;31m==> \033[97mNot running as root. Please try again as root.")
-		os.Exit(1)
-	}
 
 	fmt.Println("\033[1;32m==> \033[97mHello world!")
 
