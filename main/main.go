5b38654a009dec5edc5c91d955f395909467f5b2
trinitty999 <hanansmani@gmail.com>
2024-02-14 17:22:22 +0100

    finished startup script

--- main/main.go
+++ main/main.go
@@ -5,7 +5,7 @@ import (
 	// "github.com/diskfs/go-diskfs"
 	"os"
 	"os/exec"
-	"regexp"
+	// "encoding/json"
 )
 
 func ClearScr() {
@@ -16,32 +16,6 @@ func ClearScr() {
 
 func main() {
 	ClearScr()
-
-	fmt.Println("\033[1;32m==> \033[97mHello world!")
-
-	lscmd := exec.Command("ls", "/dev")
-	lsout, lserr := lscmd.CombinedOutput()
-
-	if lserr != nil {
-		fmt.Println("\033[1;31m==> \033[97mCommand threw error. Cannot continue.")
-		os.Exit(3)
-	}
-
-	lsoutput := string(lsout)
-	lscmd.Run()
-
-	pattern := `sd[a-z][0-9]`
-
-	re := regexp.MustCompile(pattern)
-
-	drives := re.FindStringSubmatch(lsoutput)
-
-	for _, drive := range drives {
-		// Use the regular expression to check for a match
-		if re.MatchString(drive) {
-			fmt.Printf("%s\n", drive)
-		} else {
-			fmt.Printf("%s\n", drive)
-		}
-	}
+	fmt.Println("\033[1;32m==> \033[97mHello world.")
+	os.Exit(0);
 }
